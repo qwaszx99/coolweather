d70232776b8df60cd93b8274075bd769e3a48cf5
Your Name <you@example.com>
2018-08-28 06:37:58 +0800

    添加各个天气类

---
+++ app/src/main/java/com/example/sheng/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.sheng.coolweather.gson;
+
+/**
+ * Created by Administrator on 2018/8/27.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
